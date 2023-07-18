44690e37cf4373afee13c9c7af3b017332659ee1
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-07-18 21:32:08 +0530

    Create HelloWorld.java

---
+++ OOPS (PCC-CS593)/All Assignments/Unit 1. Introduction/(2023) Lab Assignment LA1.1 - Simple computations/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+Write a Java program to display "Hello world! I am learning Java."
+Ensure to declare the main class as public. BTW, main class is the class having main() method.
+*/
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello world! I am learning Java.");
+    }
+}
